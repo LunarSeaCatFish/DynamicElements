e211bc52628bf718235eb2ac67513877d775075a
LunarSeaCatFish <DedicatedGeneric35711@outlook.com>
2024-05-16 11:51:17 -0700

    JS for last commit

--- js/main.js
+++ js/main.js
@@ -8,10 +8,15 @@ window.onload = function () {
     let submitButton = document.querySelector('button[type=submit]');
     submitButton.addEventListener('click', (event) => {
         event.preventDefault();
-        const productTitleTextBox = document.querySelector("#productTitle");
-        let title = productTitleTextBox.value;
-        const productPriceTextBox = document.querySelector("#productPrice");
-        let price = parseFloat(productPriceTextBox.value);
-        let p = new Product(title, price);
+        let p = getProduct();
+        console.log(p);
     });
 };
+function getProduct() {
+    const productTitleTextBox = document.querySelector("#productTitle");
+    let title = productTitleTextBox.value;
+    const productPriceTextBox = document.querySelector("#productPrice");
+    let price = parseFloat(productPriceTextBox.value);
+    let p = new Product(title, price);
+    return p;
+}
